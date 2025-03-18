2019cd018d7b99c9245a7c0d9b974eb5d7bc3a1c
SmolyanskyArtem <smolyansky.artem@gmail.com>
2025-03-18 22:14:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -475,7 +475,7 @@ async def on_startup(dp):
     keep_alive()
     rome = timezone("Europe/Rome")
     scheduler.add_job(send_today_plan, 'cron', hour=8, minute=00, timezone=rome)
-    scheduler.add_job(send_tomorrow_summary, 'cron', hour=20, minute=00, timezone=rome)
+    scheduler.add_job(send_tomorrow_summary, 'cron', hour=20, minute=30, timezone=rome)
     scheduler.add_job(check_reminders, 'interval', minutes=1)
     scheduler.start()
 
